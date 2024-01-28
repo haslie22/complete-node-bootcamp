843195c0e0f4728dec2b3f1083e004c9f320c2d9
Lizaveta <lizadondysh@icloud.com>
2024-01-28 04:39:22 +0200

    feat: super-basic server and routing

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -34,3 +34,39 @@ fs.readFile(path.join(__dirname, "txt", "start.txt"), "utf-8", (err, data1) => {
 });
 
 console.log("will read file");
+
+// === SERVER ===
+
+const data = fs.readFileSync(path.join(__dirname, "dev-data", "data.json"), "utf-8");
+const dataObj = JSON.parse(data);
+
+const server = http.createServer((req, res) => {
+  const pathName = req.url;
+  if (pathName === "/" || pathName === "/overview") {
+    res.end("This is the OVERVIEW!");
+  } else if (pathName === "/product") {
+    res.end("This is the PRODUCT!");
+  } else if (pathName === "/api") {
+    // reads file each time, when we navigate to /api. Better to read once and store in variable (top-level readFileSync)
+    // fs.readFile(path.join(__dirname, "dev-data", "data.json"), "utf-8", (err, data) => {
+    //   const productData = JSON.parse(data);
+    //   res.writeHead(200, {
+    //     "Content-type": "application/json",
+    //   });
+    //   res.end(data);
+    // });
+    res.writeHead(200, {
+      "Content-type": "application/json",
+    });
+    res.end(data);
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+    });
+    res.end("<h1>Page not found!</h1>");
+  }
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log(`Listening to requests on port 8000...`);
+});
